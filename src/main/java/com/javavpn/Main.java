1f56ce1d1b1f34f3e2a28f96089db87fd8ff1862
Paul Dobra <paul.dobra2005@gmail.com>
2025-07-23 20:08:14 +0300

    vpn connection successful

--- src/main/java/com/javavpn/Main.java
+++ src/main/java/com/javavpn/Main.java
@@ -3,17 +3,22 @@ package com.javavpn;
 import com.javavpn.client.VPNClient;
 import com.javavpn.server.VPNServer;
 import com.javavpn.config.VPNConfig;
+import com.javavpn.utils.LoggerUtil;
+
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = LoggerUtil.createLogger(Main.class);
+
     public static void main(String[] args) {
         VPNConfig config = new VPNConfig();
 
-        if(args.length > 0 && args[0].equalsIgnoreCase("server")) {
-            VPNServer server = new VPNServer(config);
-            server.start();
-        }else {
-            VPNClient client = new VPNClient(config);
-            client.connect();
+        if (args.length > 0 && args[0].equalsIgnoreCase("server")) {
+            logger.info("Starting VPN Server...");
+            new VPNServer(config).start();
+        } else {
+            logger.info("Starting VPN Client...");
+            new VPNClient(config).connect();
         }
     }
 }
